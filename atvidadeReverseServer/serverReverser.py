daf4a3c62ecfbe03fd6a5b034c4217ce3d10fa6f
Matheus Melo <65732203+melomatheuss@users.noreply.github.com>
2023-05-18 11:40:13 -0400

    Update serverReverser.py

--- atvidadeReverseServer/serverReverser.py
+++ atvidadeReverseServer/serverReverser.py
@@ -1,42 +1,58 @@
+'''
+MATHEUS DE SOUZA MELO
+2019007565
+ATIVIDADE MULTITHREADING
+
+'''
+
 import socket
 import threading
+import time
 
 print_lock = threading.Lock()
 
-def threaded(c):
+def handle_connection(c):
+    """
+    Função que trata a conexão com um cliente.
+
+    Args:
+        c (socket): O socket da conexão com o cliente.
+    """
     while True:
-        data = c.recv(1024)
+        data = c.recv(1024)  # Recebe os dados enviados pelo cliente
         if not data:
-            print('Conexão finalizada. Obrigado e até logo!')
             break
 
-        data = data[::-1]
+        # Simula um processamento intensivo na CPU
+        start_time = time.time()
+        while time.time() - start_time < 10:  # Processamento por 10 segundos
+            pass
 
-        c.send(data)
+        data = data[::-1]  # Inverte os dados recebidos
+        c.send(data)  # Envia os dados de volta ao cliente
 
-    c.close()
+    c.close()  # Fecha a conexão com o cliente
 
-def Main():
-    host = ""
+def main():
+    host = "localhost"
     port = 5000
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    print("Socket vinculado à porta", port)
+    backlog = 5
 
-    s.listen(5)
-    print("Socket está escutando")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))  # Associa o socket a um endereço e porta
+        print("Socket vinculado à porta", port)
 
-    while True:
-        c, addr = s.accept()
+        s.listen(backlog)  # Habilita o socket para receber conexões
+        print("Socket está escutando")
 
-        print_lock.acquire()
-        print('Conectado em:', addr[0], ':', addr[1])
+        while True:
+            c, addr = s.accept()  # Aceita uma conexão de um cliente
+            print('Conectado em:', addr[0], ':', addr[1])
 
-        thread = threading.Thread(target=threaded, args=(c,))
-        thread.start()
-
-    s.close()
+            thread = threading.Thread(target=handle_connection, args=(c,))
+            thread.start()  # Inicia uma thread para tratar a conexão
 
 if __name__ == '__main__':
-    Main()
+    main()
+
 
