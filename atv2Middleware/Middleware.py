e67ddf7e73ccb49d34ae60ffdc96b04060863e56
Matheus Melo <65732203+melomatheuss@users.noreply.github.com>
2023-05-18 10:06:45 -0400

    correção Middleware.py
    meu SO não captura os dados da CPU utilizando psutil, não sei bem o motivo.

--- atv2Middleware/Middleware.py
+++ atv2Middleware/Middleware.py
@@ -18,24 +18,44 @@ channel = connection.channel()
 channel.queue_declare(queue='cpu_queue')
 channel.queue_declare(queue='incendios_queue')
 
-#Consumidor 1
-def callback(ch, method, properties, body):
-    cpu_temp = float(body.split()[2].strip('°C'))
-    if cpu_temp > 70:
-        channel.basic_publish(exchange='incendios', routing_key='detectado', body='Fogo detectado!')
-        channel.basic_publish(exchange='prevencao', routing_key='ativar', body='Ativar sistema de prevenção!')
+# Declara a exchange
+channel.exchange_declare(exchange='temperaturas', exchange_type='direct')
 
-#Consumidor 2
+
+# Consumidor 1
+def callback(ch, method, properties, body):
+    temperatures = psutil.sensors_temperatures().get('cpu-thermal', [])
+    if temperatures:
+        cpu_temp = temperatures[0].current
+        if cpu_temp > 70:
+            channel.basic_publish(exchange='incendios', routing_key='detectado', body='Fogo detectado!')
+            channel.basic_publish(exchange='prevencao', routing_key='ativar', body='Ativar sistema de prevenção!')
+    else:
+        print('A temperatura da CPU não está disponível.')
+
+
+# Consumidor 2
 def callback_alarm(ch, method, properties, body):
-    print('Perigo, finge que tem um alarme  pra incendio tocando!!!')
+    print('Perigo, finge que tem um alarme de incêndio tocando!!!')
+
 
 channel.basic_consume(queue='cpu_queue', on_message_callback=callback, auto_ack=True)
 channel.basic_consume(queue='incendios_queue', on_message_callback=callback_alarm, auto_ack=True)
 
-while True:
 
-    cpu_temp = psutil.sensors_temperatures().get('cpu-thermal', [])[0].current
-    message = f'Temperatura da CPU: {cpu_temp} em °C'
-    channel.basic_publish(exchange='temperaturas', routing_key='cpu', body=message)
+while True:
+    temperatures = psutil.sensors_temperatures().get('cpu-thermal', [])
+    if temperatures:
+        cpu_temp = temperatures[0].current
+        message = f'Temperatura da CPU: {cpu_temp} em °C'
+        print(message)
+        channel.basic_publish(exchange='temperaturas', routing_key='cpu', body=message)
+    else:
+        message = 'A temperatura da CPU não está disponível.'
+        print(message)
+        channel.basic_publish(exchange='temperaturas', routing_key='cpu', body=message)
     time.sleep(1)
+
+# Inicie o consumidor
+channel.start_consuming()
 
