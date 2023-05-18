a91ab47d88c51fee4e46f3ce241cafb30a66d984
Matheus Melo <65732203+melomatheuss@users.noreply.github.com>
2023-05-18 15:18:17 -0400

    Update client.py

--- Trabalho4/client.py
+++ Trabalho4/client.py
@@ -1,16 +1,21 @@
 #rode o proto com o comando
-#python3 -m grpc_tools.protoc -I./ --python_out=. --grpc_python_out=. linux_machine.proto
+#python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. remote_control.proto
 
 import grpc
-import linux_machine_pb2
-import linux_machine_pb2_grpc
+import remote_control_pb2
+import remote_control_pb2_grpc
 
 def run():
     with grpc.insecure_channel("localhost:50051") as channel:
-        stub = linux_machine_pb2_grpc.LinuxMachineStub(channel)
-        command = input("Digite o comando a ser executado: ")
-        response = stub.Execute(linux_machine_pb2.CommandRequest(command=command))
-        print(response.output)
+        while True:
+            cmd = input('Digite um comando para enviar ao servidor:\n')  #Solicita ao usuário que digite um comando
+
+            # Cria um stub para o serviço RemoteControl
+            stub = remote_control_pb2_grpc.RemoteControlStub(channel)
+            response = stub.ExecCmd(remote_control_pb2.CmdRequest(cmd=cmd))  # Chama o método ExecCmd, passando a solicitação contendo o comando
+
+           # Imprime a saída do comando retornado pelo servidor
+            print(f'[localhost:50051] OUTPUT:\n{response.cmd}')
 
 if __name__ == "__main__":
     run()
